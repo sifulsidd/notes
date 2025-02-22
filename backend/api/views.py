03b641bdc7351b7a1189fa11419e30290877342f
sifulsidd <sifulsiddiki@yahoo.com>
2025-02-21 19:21:16 -0500

    Delete and create views done

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,48 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
+
+# cannot call route unless authenticated
+# lists all the notes user created
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        # if we wanted all notes can do: Note.objects.all()
+        
+        # filters by author field, shows all notes by this specific user
+        return Note.objects.filter(author=user)
 
 
+    # reference Django documentation to figure out what functions we have to override
+
+    # we want to do custom configurations when creating new user
+    # override create method
+    
+    # serializer is passed data 
+    # get access to serializer data and check if it is valid
+    def perform_create(self, serializer):
+        # if valid, makes a new version of the note
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+    
+# specifies note we want to delete, deletes for us if we are authenticated
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+ 
+        
 class CreateUserView(generics.CreateAPIView):
     # list of different objects when create new one, make sure don't create user that exists
     queryset = User.objects.all()
@@ -12,5 +50,4 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
     # who can call this data, think registration
     permission_classes = [AllowAny]
-    
     
