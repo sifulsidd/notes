5f53cf53f9dbd9fa92e3f1c04eff2fc393210953
sifulsidd <sifulsiddiki@yahoo.com>
2025-02-24 10:51:08 -0500

    backend fixed

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -9,6 +9,8 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {"password": {"write_only": True}}
     
     def create(self, validated_data):
+        # prints the username and password in backend when creating user
+        print(validated_data)
         user = User.objects.create_user(**validated_data)
         return user
 
