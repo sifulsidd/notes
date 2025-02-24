b1bd369f676957e12f0c4fa0f685ee623e322ca4
sifulsidd <sifulsiddiki@yahoo.com>
2025-02-24 14:50:50 -0500

    Trying to add a view to Notes, to get a specific view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path("notes/", views.NoteListCreate.as_view(), name="note-list"),
     path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
     
+    # find a specific note
+    path("notes/<int:id>/", views.NoteView.as_view(), name="view-note"),
+    # add an edit path
+    path("notes/edit/<int:pk>/", views.NoteEdit.as_view(), name="edit-note")
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from rest_framework import generics
+from rest_framework import generics, status
 from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Note
+from rest_framework.response import Response
 
 # cannot call route unless authenticated
 # lists all the notes user created
@@ -51,3 +52,21 @@ class CreateUserView(generics.CreateAPIView):
     # who can call this data, think registration
     permission_classes = [AllowAny]
     
+class NoteView(generics.RetrieveAPIView):
+    def get(self, request, id):
+        try:
+            note = Note.objects.get(id=id)
+        except Note.DoesNotExist: 
+            print("Note not found in database")
+            
+        serializer = NoteSerializer(note)
+        return Response(serializer.data, status= status.HTTP_200_OK)
+
+class NoteEdit(generics.UpdateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+    
+    # this makes sure only the information that user has access to can be accessed   
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
