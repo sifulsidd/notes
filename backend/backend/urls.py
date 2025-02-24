64cbf9e4b0ff66d876554037af860bb80e196e61
sifulsidd <sifulsiddiki@yahoo.com>
2025-02-24 10:30:23 -0500

    Routes figured out

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -23,7 +23,11 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/register/', CreateUserView.as_view(), name='register'),
+    
+    # this token verfies token and logs us in
     path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
+    
+    
     path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
     path('api-auth/', include('rest_framework.urls')),
     
